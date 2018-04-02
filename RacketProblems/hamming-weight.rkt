c1b434f540b6dd9b27bd8b1bca162405c0e0a873
Robert Kiliszewski <robert.kiliszewski@hotmail.com>
2018-04-02 16:25:59 +0100

    Hamming Weight

--- RacketProblems/hamming-weight.rkt
+++ RacketProblems/hamming-weight.rkt
@@ -1,2 +1,23 @@
 #lang racket
 
+;;https://stackoverflow.com/questions/32854125/racket-how-to-count-the-number-of-elements-in-a-list
+
+(define (hamming-weight binarylst)
+  
+  ;;if null do nothing 
+  (if(null? binarylst)
+     
+     0
+     
+     ;;if not null do the following
+     (if(=(car binarylst) 0 )
+        
+        ;;check if number is not 0
+        (hamming-weight (cdr binarylst))
+        
+        ;;if its not 0 then add the 1's together 
+        (+ 1 (hamming-weight(cdr binarylst))))))
+
+(hamming-weight '(1 0 1 0 1 1 1 0))
+  
+
