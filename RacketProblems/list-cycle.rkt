b09f9d1b6f8cf5c709afc7812de4cb1e5d9a1705
Robert Kiliszewski <robert.kiliszewski@hotmail.com>
2018-04-02 14:17:57 +0100

    Right Cycle Problem Added to List Cycle Problem Sheet

--- RacketProblems/list-cycle.rkt
+++ RacketProblems/list-cycle.rkt
@@ -1,9 +1,29 @@
+;;https://stackoverflow.com/questions/13046017/rotate-a-list-to-the-left-in-scheme-racket
+
 #lang racket
 
 (define (lcycle x)
-  (if ( null? x ) '()
-      (cons(cdr x)
+  (if ( null? x )'()
+     
+      (cons (cdr x)
+
              (cons(car x)'()))))
+
+;;https://stackoverflow.com/questions/5006750/removing-last-element-of-a-listscheme
+;;https://stackoverflow.com/questions/13175152/scheme-getting-last-element-in-list
+
+(define (remove-last-object x)
+    (if (null? (cdr x))
+       '()
+        (cons (car x) (remove-last-object (cdr x)))))
+
+(define (read-last-object y)
+  (cond ((null? (cdr y)) (car y))
+        (else (read-last-object (cdr y)))))
+
+(define(rcycle z)
+  (cons (read-last-object z) (remove-last-object  z)))
 
 
 (lcycle '(1 2 3 4 5))
+(rcycle '(1 2 3 4 5))
